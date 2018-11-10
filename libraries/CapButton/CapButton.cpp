33fd61f0eda6dab14fcf35abb23faa4d5729fd62
Anders <43926857+AnderBHC@users.noreply.github.com>
2018-11-10 14:22:46 +0100

    Pins, lauffähig

--- libraries/CapButton/CapButton.cpp
+++ libraries/CapButton/CapButton.cpp
@@ -2,41 +2,17 @@
 #include "CapButton.h"
 #include "CapacitiveSensor.h"
 
-CapButton::CapButton(uint8_t pinRead, uint8_t pinSend){
-	_pinRead = pinRead;
-	_pinSend = pinSend;
 CapButton::CapButton(CapacitiveSensor& Sensor){
-
+	_Sensor = &Sensor;
 	lastDebounce = 0;
 	oldstate = LOW;
 	clickStart = 0;
 	clickEnded = true;
 	longclicked = false;
 	lastClick = 0;
-	debouncetime = 100;
-	CapacitiveSensor Sensor= CapacitiveSensor (_pinSend, _pinRead);
-}
-
-int CapButton::update(){
-<<<<<<< HEAD
-	int state = Sensor.capacitiveSensor(30);
-
-=======
-	long total=_Sensor.capacitiveSensor(100);
-	
-	int state = LOW;
-	if (total>50){
-		state=HIGH;
-	}
-/* 	Serial.print(total);
-	Serial.print("\t");
-	Serial.print(state);
-	Serial.print("\n");
- */
-=======
 	debouncetime = 50;
-	_Sensor = &Sensor;
-	treshhold = _Sensor -> capacitiveSensor(100)*1.1;
+	treshhold = 1000;
+
 }
 
 long CapButton::readRaw(){
@@ -50,9 +26,9 @@ int CapButton::update(){
 	if (total > treshhold){
 		state = HIGH;
 	}
+	Serial.print(total);
+	Serial.print("\t");
 
->>>>>>> c4275df3c6038977776678ea4fd15f65719ed85b
->>>>>>> parent of d65e85d... Revert "Merge branch 'master' of https://github.com/AnderBHC/Super-Bierpong"
 	//steigende flanke
 	if (state == HIGH && millis() - lastDebounce > debouncetime && clickEnded == true) {
 		clickStart = millis();
@@ -62,39 +38,23 @@ int CapButton::update(){
 	//Prüft, ob ein klicken + halten da ist.
 	if (state == HIGH && clickEnded == false && millis() - clickStart > 500 && longclicked == false){
 		longclicked = true;
-		return 2; //klicken + halten
+		return 1; //klicken + halten
 
 	}
 	//fallende flanke
 	if (state == LOW && clickEnded == false && millis() - lastDebounce > debouncetime){
 		clickEnded = true;
-		lastDebounce = millis();
-<<<<<<< HEAD
-		lastClick = millis();
-=======
-		
-=======
-		//lastDebounce = millis();
->>>>>>> c4275df3c6038977776678ea4fd15f65719ed85b
->>>>>>> parent of d65e85d... Revert "Merge branch 'master' of https://github.com/AnderBHC/Super-Bierpong"
 		if (longclicked == true){
 			longclicked = false;
 			return 0;
 			}
-<<<<<<< HEAD
 		if (clickStart - lastClick < 500){
-=======
-<<<<<<< HEAD
-		if (clickStart - lastClick < 100){
-=======
-		if (clickStart - lastClick < 500){
->>>>>>> c4275df3c6038977776678ea4fd15f65719ed85b
 			lastClick = millis();
->>>>>>> parent of d65e85d... Revert "Merge branch 'master' of https://github.com/AnderBHC/Super-Bierpong"
 			return 3; //doppel klick
 		}
 		else{
-			return 1; // normaler klick
+			lastClick = millis();
+			return 0; // normaler klick
 		}
 	}
 
--- super-Bierpong.ino
+++ super-Bierpong.ino
@@ -6,26 +6,25 @@
 
 
 //Pinbelegung
-const int PinStripTriA = 2;
-const int PinStripTriB = 5;
+const int PinStripTriA = 5;
+const int PinStripTriB = 2;
 const int PinStripSideR = 3;
 const int PinStripSideL = 4;
 
-const int PinSend = 6;
-const int PinButtonModus = 7;
-const int PinButtonColor = 8;
-const int PinButtonUmstellenA = 9;
-const int PinButtonUmstellenB = 10;
+const int PinSend = 12;
+const int PinButtonModus = 6;
+const int PinButtonUmstellenA = 7;
+const int PinButtonUmstellenB = 8;
 
 //konstruktor für den LED streifen (Anzahl LEDs, Angeschlossener PIN, Modus)
-const int PixelStripSideR = 95;
-const int PixelStripSideL= 95;
+const int PixelStripSideR = 94;
+const int PixelStripSideL= 94;
 const int PixelStripTriA= 40;
 const int PixelStripTriB = 40;
 Adafruit_NeoPixel StripSideL = Adafruit_NeoPixel (PixelStripSideL, PinStripSideL, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel StripSideR = Adafruit_NeoPixel (PixelStripSideR, PinStripSideR, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel StripTriA = Adafruit_NeoPixel (PixelStripTriA, PinStripTriA, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel StripTriB = Adafruit_NeoPixel (PixelStripTriB, PinStripTriB, NEO_RGB + NEO_KHZ800);
+Adafruit_NeoPixel StripTriB = Adafruit_NeoPixel (PixelStripTriB, PinStripTriB, NEO_GRB + NEO_KHZ800);
 
 //Konstruktor für die Sensoren CapacitiveSensor(send,read)
 CapacitiveSensor SensorModus = CapacitiveSensor(PinSend, PinButtonModus);
@@ -35,7 +34,6 @@ CapacitiveSensor SensorUmstellenB = CapacitiveSensor(PinSend, PinButtonUmstellen
 CapButton ButtonModus = CapButton(SensorModus);
 CapButton ButtonUmstellenA = CapButton(SensorUmstellenA);
 CapButton ButtonUmstellenB = CapButton(SensorUmstellenB);
-//CapButton ButtonColor = CapButton();
 
 uint8_t Modus = 0; //0 Bierpong normal | 1 Bierpong Rainbow |  2 Bierpong Kings Cup | 3 Flipcup
 uint8_t oldModus = 0;
@@ -43,8 +41,8 @@ uint8_t PunkteTeamA = 0; //0 alle | 1 Blume | 2 diamant | 3 aus
 uint8_t PunkteTeamB = 0; //0 alle | 1 Blume | 2 diamant | 3 aus
 
 //addressern der Pixel in den einzelnen Bierpong Leuchtfeldern von oben nach unten von links nach rechts
-byte FeldA[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{15,14,0,1},{13,14,2,3},{11,10,4,5},{9,8,6,7}};
-byte FeldB[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{15,14,7,6},{13,12,5,4},{11,10,3,2},{9,8,1,0}};
+byte FeldA[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{6,7,14,15},{4,5,12,13},{2,3,10,11},{0,1,8,9}};
+byte FeldB[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{0,1,14,15},{2,3,12,13},{4,5,10,11},{6,7,8,9}};
 
 //faktoren für einzelne Bierpong felder nach dem umstellen
 byte Blume[11]={0,1,1,1,1,1,0,1,1,0};
@@ -81,7 +79,6 @@ void loop() {
   stateModusButton = ButtonModus.update();
   stateUmstellenButtonA = ButtonUmstellenA.update();
   stateUmstellenButtonB = ButtonUmstellenB.update();
-  stateColorButton = ButtonColor.update();
 
   if (stateModusButton == 1){
     Modus ++;
@@ -118,7 +115,6 @@ void loop() {
   StripTriB.show();
   StripSideL.show();
   StripSideR.show();
-
 }
 
 void Bierpong(){
@@ -127,13 +123,13 @@ void Bierpong(){
     PunkteTeamA = 0;
     PunkteTeamB = 0;
   }
-  if (stateUmstellenButtonA == 1){
+  if (stateUmstellenButtonB == 1){
     PunkteTeamB ++;
   }
   if (PunkteTeamB > 3){
     PunkteTeamB = 0;
   }
-  if (stateUmstellenButtonB == 1){
+  if (stateUmstellenButtonA == 1){
     PunkteTeamA ++;
   }
   if (PunkteTeamA > 3){
@@ -143,22 +139,24 @@ void Bierpong(){
     case(0):
       for (int i = 0; i<10; i++){
         for ( int j = 0; j < 4; j++){
-          StripTriA.setPixelColor(FeldA[i][j], 0, 255, 0);
+          StripTriA.setPixelColor(FeldA[i][j], 255, 200, 0);
       }
     }
+	break;
     case(1):
       for ( int i = 0; i < 10; i++ ){
         for ( int j = 0; j < 4; j++){
-          StripTriA.setPixelColor(FeldA[i][j], 127*Blume[i], 127*Blume[i], 0);
+          StripTriA.setPixelColor(FeldA[i][j], 0, 255*Blume[i], 255*Blume[i]);
         }
       }
+	break;
     case(2):
       for ( int i = 0; i < 10; i++ ){
         for ( int j = 0; j < 4; j++){
-          StripTriA.setPixelColor(FeldA[i][j], 255*Diamant[i], 0, 0);
+          StripTriA.setPixelColor(FeldA[i][j], 255*Diamant[i], 0, 255*Diamant[i]);
         }
       }
-
+		break;
   }
   switch (PunkteTeamB){
     case(0):
@@ -167,18 +165,21 @@ void Bierpong(){
           StripTriB.setPixelColor(FeldB[i][j], 0, 255, 0);
       }
     }
+	break;
     case(1):
       for ( int i = 0; i < 10; i++ ){
         for ( int j = 0; j < 4; j++){
           StripTriB.setPixelColor(FeldB[i][j], 127*Blume[i], 127*Blume[i], 0);
         }
       }
+	break;
     case(2):
       for ( int i = 0; i < 10; i++ ){
         for ( int j = 0; j < 4; j++){
           StripTriB.setPixelColor(FeldB[i][j], 255*Diamant[i], 0*Diamant[i], 0);
         }
       }
+	break;
   }
 }
 
@@ -194,10 +195,11 @@ void Kingscup(){
       TrackerRandomFieldB[i] = 0;
     }
   }
-
+    int breakA;
+	int breakB;
   if (stateUmstellenButtonA == 1){
+	  breakA = 0;
     RandomFieldA = rand() %(9-0)+0; //Generates number between 0 - 9
-    int breakA = 0;
     while (TrackerRandomFieldA[RandomFieldA] == 1 && breakA < 10){
       RandomFieldA++;
       breakA++;
@@ -219,7 +221,7 @@ void Kingscup(){
 }
   if (stateUmstellenButtonB == 1){
     RandomFieldB = rand() %(9-0)+0; //Generates number between 0 - 9
-    int breakB = 0;
+    breakB = 0;
     while (TrackerRandomFieldB[RandomFieldB] == 1 && breakB < 10){
       RandomFieldB++;
       breakB++;
@@ -235,7 +237,7 @@ void Kingscup(){
   }
   if (PunkteTeamB > 0){
     PunkteTeamB = 0;
-    for int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++){
       TrackerRandomFieldB[i] = 0;
     }
   }
@@ -268,7 +270,7 @@ void Kingscup(){
     default:
       for ( int i = 0; i < 10; i++ ){
         for ( int j = 0; j < 4; j++){
-          StripTriB.setPixelColor(FeldB[i][j], 255*SetRandomFieldB[i], 255*SetRandomFieldB[i], 255*SetRandomFieldA[i]);
+          StripTriB.setPixelColor(FeldB[i][j], 255*SetRandomFieldB[i], 255*SetRandomFieldB[i], 255*SetRandomFieldB[i]);
         }
       }
     break;
@@ -281,16 +283,16 @@ void Rainbow(){
     PunkteTeamA = 0;
     PunkteTeamB = 0;
   }
-  if (stateUmstellenButtonA == 1){
+  if (stateUmstellenButtonB == 1){
     PunkteTeamB ++;
   }
-  if (PunkteTeamB > 3){
+  if (PunkteTeamB > 2){
     PunkteTeamB = 0;
   }
-  if (stateUmstellenButtonB == 1){
+  if (stateUmstellenButtonA == 1){
     PunkteTeamA ++;
   }
-  if (PunkteTeamA > 3){
+  if (PunkteTeamA > 2){
     PunkteTeamA = 0;
   }
   switch (PunkteTeamA){
@@ -300,22 +302,24 @@ void Rainbow(){
         for ( int j = 0; j < 4; j++){
           StripTriA.setPixelColor(FeldA[i][j], RainbowRot(offset), RainbowGruen(offset), RainbowBlau(offset));
       }
-    }
+      }
+	  break;
     case(1):
       for ( int i = 0; i < 10; i++ ){
-        int offset = i*766/10;
+        int offset = i*766/10+(millis()/4);
         for ( int j = 0; j < 4; j++){
           StripTriA.setPixelColor(FeldA[i][j], RainbowRot(offset)*Blume[i], RainbowGruen(offset)*Blume[i], RainbowBlau(offset)*Blume[i]);
         }
       }
+	  break;
     case(2):
       for ( int i = 0; i < 10; i++ ){
-        int offset = i*766/10;
+        int offset = i*766/10+(millis()/2);
         for ( int j = 0; j < 4; j++){
           StripTriA.setPixelColor(FeldA[i][j], RainbowRot(offset)*Diamant[i], RainbowGruen(offset)*Diamant[i], RainbowBlau(offset)*Diamant[i]);
         }
       }
-
+	  break;
   }
   switch (PunkteTeamB){
     case(0):
@@ -325,20 +329,23 @@ void Rainbow(){
           StripTriB.setPixelColor(FeldB[i][j],RainbowRot(offset) , RainbowGruen(offset), RainbowBlau(offset));
       }
     }
+	break;
     case(1):
       for ( int i = 0; i < 10; i++ ){
-        int offset = i*766/10;
+        int offset = i*766/10+(millis()/4);
         for ( int j = 0; j < 4; j++){
           StripTriB.setPixelColor(FeldB[i][j], RainbowRot(offset)*Blume[i], RainbowGruen(offset)*Blume[i], RainbowBlau(offset)*Blume[i]);
         }
       }
+	  break;
     case(2):
       for ( int i = 0; i < 10; i++ ){
-        int offset = i*766/10;
+        int offset = i*766/10+(millis()/2);
         for ( int j = 0; j < 4; j++){
           StripTriB.setPixelColor(FeldB[i][j], RainbowRot(offset)*Diamant[i], RainbowGruen(offset)*Diamant[i], RainbowBlau(offset)*Diamant[i]);
         }
       }
+	  break;
   }
   Flipcup();
 }
@@ -348,9 +355,9 @@ void Flipcup(){
     oldModus = Modus;
   }
   for (int i = 0; i < PixelStripSideL; i++){
-    long unsigned int offset = i*766/PixelStripSideL/2+millis()/1000;
+    long unsigned int offset = i+(millis()/100);
     StripSideL.setPixelColor(i, RainbowRot(offset) ,RainbowGruen(offset), RainbowBlau(offset));
-    StripSideR.setPixelColor(94-i, RainbowRot(offset), RainbowGruen(offset), RainbowBlau(offset));
+    StripSideR.setPixelColor(i, RainbowRot(offset), RainbowGruen(offset), RainbowBlau(offset));
   }
 }
 
@@ -362,7 +369,7 @@ byte RainbowRot(unsigned int offset){
   if (c > 255 && c <= 510) {
     return 0;
   }
-  if (c > 510 and c <= 765){
+  if (c > 510){
     return c - 510;
   }
 }
@@ -387,7 +394,7 @@ byte RainbowBlau(unsigned int offset){
   if (c>=256 && c<510){
     return c - 255;
   }
-  if (c >= 510 && c< 765){
+  if (c >= 510){
     return 765 - c;
   }
 }
