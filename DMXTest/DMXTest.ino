b9a321efb6d08e162a5651e00ec48a6ed881270d
Anders <43926857+AnderBHC@users.noreply.github.com>
2018-12-19 14:51:11 +0100

    dmxprobe

--- DMXTest/DMXTest.ino
+++ DMXTest/DMXTest.ino
@@ -1,55 +1,27 @@
 #include <DMXSerial.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+uint8_t *RXBuffer;
+Adafruit_NeoPixel Strip=Adafruit_NeoPixel(40, 2, NEO_GRB + NEO_KHZ800);
 
 void setup(){
-  DMXSerial.init(DMXController);
-  DMXSerial.maxChannel(66);
+  Strip.begin();
+  Strip.clear();
+  DMXSerial.init(DMXProbe,13);
+  RXBuffer = DMXSerial.getBuffer();
+
 }
 
-long last = 0;
-unsigned int c = 0;
+
 void loop(){
-  for(int i = 1; i <= 66, i = i + 3){
-    int offset = i*766/10+(millis()/4);
-    DMXSerial.write(i, RainbowRot(offset));
-    DMXSerial.write(i+1,RainbowGruen(offset));
-    DMXSerial.write(i+2,RainbowBlau(offset));
-  }
-}
 
-byte RainbowRot(unsigned int offset){
-  int c = offset%766;
-  if (c >= 0 && c <= 255){
-    return 255 - c;
+  if (DMXSerial.receive()){
+    for(int i = 0; i < 22; i++)
+    Strip.setPixelColor(i,RXBuffer[i*3+1],RXBuffer[i*3+2],RXBuffer[i*3+3]);
   }
-  if (c > 255 && c <= 510) {
-    return 0;
-  }
-  if (c > 510){
-    return c - 510;
-  }
-}
-byte RainbowGruen(unsigned int offset){
-  int c = offset%766;
-  byte color;
-  if (c >= 0 && c <= 255){
-    return c;
-  }
-  if (c > 255 && c <= 510){
-    return 510 - c;
-  }
-  if(c > 510){
-    return 0;
-  }
-}
-byte RainbowBlau(unsigned int offset){
-  int c = offset%766;
-  if(c >= 0 && c <= 255){
-    return 0;
-  }
-  if (c>=256 && c<510){
-    return c - 255;
-  }
-  if (c >= 510){
-    return 765 - c;
-  }
+
+    Strip.show();
 }
