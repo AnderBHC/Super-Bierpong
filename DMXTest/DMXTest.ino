210ad2561256a9991a9218768c0a415ee6a1eacb
Anders <43926857+AnderBHC@users.noreply.github.com>
2018-12-19 16:29:50 +0100

    4 Strip working

--- DMXTest/DMXTest.ino
+++ DMXTest/DMXTest.ino
@@ -4,12 +4,41 @@
   #include <avr/power.h>
 #endif
 
+#define PinStripTriA 5
+#define PinStripTriB  2
+#define PinStripSideR  3
+#define PinStripSideL  4
+
+#define PixelStripSide 94
+#define PixelStripTri 40
+
 uint8_t *RXBuffer;
-Adafruit_NeoPixel Strip=Adafruit_NeoPixel(22, 2, NEO_GRB + NEO_KHZ800);
+
+Adafruit_NeoPixel StripSideL = Adafruit_NeoPixel (PixelStripSide, PinStripSideL, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel StripSideR = Adafruit_NeoPixel (PixelStripSide, PinStripSideR, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel StripTriA = Adafruit_NeoPixel (PixelStripTri, PinStripTriA, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel StripTriB = Adafruit_NeoPixel (PixelStripTri, PinStripTriB, NEO_GRB + NEO_KHZ800);
+
+
+const byte FeldB[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{0,1,14,15},{2,3,12,13},{4,5,10,11},{6,7,8,9}};
+const byte FeldA[11][5]={{36,37,39,38},{35,34,28,29},{33,32,30,31},{27,26,16,17},{25,24,18,19},{23,22,20,21},{6,7,14,15},{4,5,12,13},{2,3,10,11},{0,1,8,9}};
 
 void setup(){
-  Strip.begin();
-  Strip.clear();
+  StripTriA.begin();
+  StripTriB.begin();
+  StripSideL.begin();
+  StripSideR.begin();
+
+  StripTriA.clear();
+  StripTriB.clear();
+  StripSideL.clear();
+  StripSideR.clear();
+
+  StripTriA.show();
+  StripTriB.show();
+  StripSideL.show();
+  StripSideR.show();
+
   DMXSerial.init(DMXProbe,13);
   RXBuffer = DMXSerial.getBuffer();
 
@@ -20,9 +49,35 @@ void loop(){
 
   if (DMXSerial.receive()){
     for (int i = 0; i < 22; i++)
-    Strip.setPixelColor(i,RXBuffer[i*3+1],RXBuffer[i*3+2],RXBuffer[i*3+3]);
+    setFieldColor(i,RXBuffer[i*3+1],RXBuffer[i*3+2],RXBuffer[i*3+3]);
 
   }
-    Strip.show();
+    StripTriA.show();
+    StripTriB.show();
+    StripSideL.show();
+    StripSideR.show();
 
+}
+
+void setFieldColor(uint8_t field, uint8_t red, uint8_t green, uint8_t blue){
+  if (field < 10){
+    for (int i = 0; i < 4; i++){
+      StripTriA.setPixelColor(FeldA[field][i], red, green, blue);
+    }
+  }
+    else if (field < 20){
+      for(int i = 0; i < 4; i++){
+        StripTriB.setPixelColor(FeldB[field-10][i], red, green, blue);
+      }
+    }
+    else if (field == 20){
+      for (int i = 0; i < PixelStripSide; i++){
+        StripSideL.setPixelColor(i, red, green, blue);
+      }
+    }
+    else if (field == 21){
+      for(int i = 0; i < PixelStripSide; i++){
+        StripSideR.setPixelColor(i, red, green, blue);
+      }
+    }
 }
